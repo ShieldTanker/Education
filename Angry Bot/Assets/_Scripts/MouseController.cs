dc8665ce157219ff2df976bad78d4aa85388d7af
ShieldTanker <eogus2346@naver.com>
2024-05-08 17:48:13 +0900

    마우스 커서 설명문 추가

--- Angry Bot/Assets/_Scripts/MouseController.cs
+++ Angry Bot/Assets/_Scripts/MouseController.cs
@@ -18,10 +18,11 @@ public class MouseController : MonoBehaviour
         // RayCast 는 bool 타입임 물체에 닿으면 true 아니면 false
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            // 부딫힌 위치는 hit 에 저장 되어 있음 (hit.pint.x, 1f (물체에 겹치면 보일수있어 0.5f 만큼 띄움), hit.point.z)
+            // 부딫힌 위치는 hit 에 저장 되어 있음 (hit.pint.x, 1f (물체에 겹쳐 보일수있어 0.5f 만큼 띄움), hit.point.z)
             cursor.transform.position = new Vector3(hit.point.x, 0.5f, hit.point.z);
             // hit.collider -> 오브젝트의 콜라이더 객체
 
+            // 마우스 방향으로 사격
             if (Input.GetMouseButtonDown(0) && playerCtrl.playerState != PlayerState.Dead)
             {
                 target.position = new Vector3(hit.point.x, 0f, hit.point.z);
