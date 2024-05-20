a6156bdbf6f8af73c13bd920931ec6e3f6c4702d
ShieldTanker <eogus2346@naver.com>
2024-05-20 10:47:26 +0900

    유한 상태 기계 1일차

---
+++ TankFSM/Assets/_Scripts/Target.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Target : MonoBehaviour
+{
+    public NavMeshAgent playerNavAgent;
+
+    private void Start()
+    {
+        playerNavAgent = GameObject.FindWithTag("Player").GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray.origin, ray.direction ,out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                playerNavAgent.destination = targetPosition;
+                transform.position = targetPosition;
+            }
+        }
+    }
+}
